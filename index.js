208031544329c4d02a595e5f652e539ed1495daf
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2023-02-15 13:35:06 -0800

    build(deps): Update to @launchdarkly/openfeature-node-server version 0.2.0. (#2)

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ const sdkKey = '';
 // Set featureFlagKey to the feature flag key you want to evaluate
 const featureFlagKey = 'my-boolean-flag';
 
-// Set up the user properties. This user should appear on your LaunchDarkly users dashboard
+// Set up the context properties. This context should appear on your LaunchDarkly contexts dashboard
 // soon after you run the demo.
 // Remember when using OpenFeature to use `targetingKey` instead of `key`.
 const context = {
-  targetingKey: 'example-user-key'
+  targetingKey: 'example-user-key',
+  kind: 'user'
 };
 
 const ldClient = init(sdkKey);
@@ -23,11 +24,11 @@ const client = OpenFeature.getClient();
 
 const flagValue = await client.getBooleanValue(featureFlagKey, false, context);
 
-console.log(`Feature flag '${featureFlagKey}' is ${flagValue} for this user`);
+console.log(`Feature flag '${featureFlagKey}' is ${flagValue} for this context`);
 
 // Here we ensure that the SDK shuts down cleanly and has a chance to deliver analytics
 // events to LaunchDarkly before the program exits. If analytics events are not delivered,
-// the user properties and flag usage statistics will not appear on your dashboard. In a
+// the context properties and flag usage statistics will not appear on your dashboard. In a
 // normal long-running application, the SDK would continue running and events would be
 // delivered automatically in the background.
 await ldClient.flush();
