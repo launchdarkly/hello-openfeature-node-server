79e4218ab60ede28ae9ecde6128c999a8cbae380
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2023-03-15 09:23:48 -0700

    Put in function.

--- src/index.ts
+++ src/index.ts
@@ -20,20 +20,24 @@ class AnonymousDefaultContext implements Hook {
 
 OpenFeature.addHooks(new AnonymousDefaultContext());
 
-const ldClient = init(sdkKey);
-await ldClient.waitForInitialization();
+async function runExample() {
+  const ldClient = init(sdkKey);
+  await ldClient.waitForInitialization();
 
-OpenFeature.setProvider(new LaunchDarklyProvider(ldClient));
-const client = OpenFeature.getClient();
+  OpenFeature.setProvider(new LaunchDarklyProvider(ldClient));
+  const client = OpenFeature.getClient();
 
-const flagValue = await client.getBooleanValue(featureFlagKey, false);
+  const flagValue = await client.getBooleanValue(featureFlagKey, false);
 
-console.log(`Feature flag '${featureFlagKey}' is ${flagValue} for this context`);
+  console.log(`Feature flag '${featureFlagKey}' is ${flagValue} for this context`);
 
-// Here we ensure that the SDK shuts down cleanly and has a chance to deliver analytics
-// events to LaunchDarkly before the program exits. If analytics events are not delivered,
-// the context properties and flag usage statistics will not appear on your dashboard. In a
-// normal long-running application, the SDK would continue running and events would be
-// delivered automatically in the background.
-await ldClient.flush();
-ldClient.close();
+  // Here we ensure that the SDK shuts down cleanly and has a chance to deliver analytics
+  // events to LaunchDarkly before the program exits. If analytics events are not delivered,
+  // the context properties and flag usage statistics will not appear on your dashboard. In a
+  // normal long-running application, the SDK would continue running and events would be
+  // delivered automatically in the background.
+  await ldClient.flush();
+  ldClient.close();
+}
+
+runExample();
