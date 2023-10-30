64e0ef29902f7ac7f32fd7e4724b5231e83e83da
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2023-10-30 13:36:35 -0700

    feat: Update to work with latest OpenFeature provider release.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-import { OpenFeature } from '@openfeature/js-sdk';
-import { init } from 'launchdarkly-node-server-sdk';
+import { OpenFeature, ProviderEvents } from '@openfeature/server-sdk';
 import { LaunchDarklyProvider } from '@launchdarkly/openfeature-node-server';
 
 // Set sdkKey to your LaunchDarkly SDK key.
@@ -16,20 +15,19 @@ const context = {
   kind: 'user'
 };
 
-const ldClient = init(sdkKey);
-await ldClient.waitForInitialization();
+OpenFeature.setProvider(new LaunchDarklyProvider(sdkKey));
 
-OpenFeature.setProvider(new LaunchDarklyProvider(ldClient));
-const client = OpenFeature.getClient();
+OpenFeature.addHandler(ProviderEvents.Ready, async (_eventDetails) => {
+  const client = OpenFeature.getClient();
 
-const flagValue = await client.getBooleanValue(featureFlagKey, false, context);
-
-console.log(`Feature flag '${featureFlagKey}' is ${flagValue} for this context`);
-
-// Here we ensure that the SDK shuts down cleanly and has a chance to deliver analytics
-// events to LaunchDarkly before the program exits. If analytics events are not delivered,
-// the context properties and flag usage statistics will not appear on your dashboard. In a
-// normal long-running application, the SDK would continue running and events would be
-// delivered automatically in the background.
-await ldClient.flush();
-ldClient.close();
+  const flagValue = await client.getBooleanValue(featureFlagKey, false, context);
+  
+  console.log(`Feature flag '${featureFlagKey}' is ${flagValue} for this context`);
+  
+  // Here we ensure that the SDK shuts down cleanly and has a chance to deliver analytics
+  // events to LaunchDarkly before the program exits. If analytics events are not delivered,
+  // the context properties and flag usage statistics will not appear on your dashboard. In a
+  // normal long-running application, the SDK would continue running and events would be
+  // delivered automatically in the background.
+  await OpenFeature.close();
+});
