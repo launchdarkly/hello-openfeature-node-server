767470ef0da1f211ffebf669b367ef9feb6d87dd
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2024-06-04 15:11:07 -0700

    feat: Update to latest versions.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { OpenFeature, ProviderEvents } from '@openfeature/server-sdk';
 import { LaunchDarklyProvider } from '@launchdarkly/openfeature-node-server';
 
 // Set sdkKey to your LaunchDarkly SDK key.
-const sdkKey = '';
+const sdkKey = process.env.LAUNCHDARKLY_SDK_KEY ?? '';
 
 // Set featureFlagKey to the feature flag key you want to evaluate
 const featureFlagKey = 'my-boolean-flag';
@@ -15,15 +15,15 @@ const context = {
   kind: 'user'
 };
 
-OpenFeature.setProvider(new LaunchDarklyProvider(sdkKey));
+await OpenFeature.setProviderAndWait(new LaunchDarklyProvider(sdkKey));
 
 OpenFeature.addHandler(ProviderEvents.Ready, async (_eventDetails) => {
   const client = OpenFeature.getClient();
 
   const flagValue = await client.getBooleanValue(featureFlagKey, false, context);
-  
+
   console.log(`Feature flag '${featureFlagKey}' is ${flagValue} for this context`);
-  
+
   // Here we ensure that the SDK shuts down cleanly and has a chance to deliver analytics
   // events to LaunchDarkly before the program exits. If analytics events are not delivered,
   // the context properties and flag usage statistics will not appear on your dashboard. In a
